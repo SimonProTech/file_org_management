46f2fc0ac40185e8dd819c7c074e1194129540e1
Szymon <joceksz@wp.pl>
2024-03-03 16:46:12 +0100

    created bin route

---
+++ src/app/files/bin/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FileHeader from '@/app/components/common/FileHeader';
+
+const Page = () => (
+  <div>
+    <FileHeader title="Trash files" />
+  </div>
+);
+
+export default Page;
