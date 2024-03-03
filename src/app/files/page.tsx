8b12ca3bf2751b4c6c32e203e77a5bee58f60304
Szymon <joceksz@wp.pl>
2024-03-03 16:46:42 +0100

    created files route

---
+++ src/app/files/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AllFiles from '@/app/components/files/AllFiles';
+
+const Files = () => (
+  <div className="flex flex-col flex-1 relative">
+    <AllFiles />
+  </div>
+);
+
+export default Files;
