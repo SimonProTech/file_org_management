a3928ad5d84eaa5784accd9852440437d2118245
Szymon <joceksz@wp.pl>
2024-03-09 19:10:07 +0100

    props fixed

--- src/app/components/HeaderCreateOrganization.tsx
+++ src/app/components/HeaderCreateOrganization.tsx
@@ -3,7 +3,6 @@
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog';
@@ -14,11 +13,12 @@ import CreateOrganizationForm from '@/app/components/forms/CreateOrganizationFor
 interface HeaderCreateOrganizationProps {
     openDialog: boolean;
     setOpenDialog: (x: boolean) => void;
+    id: string;
 }
 
 const HeaderCreateOrganization:
     FC<HeaderCreateOrganizationProps> = ({
-      setOpenDialog, openDialog,
+      setOpenDialog, openDialog, id,
     }) => (
       <Dialog
         onOpenChange={setOpenDialog}
@@ -28,9 +28,8 @@ const HeaderCreateOrganization:
           <DialogHeader>
             <DialogTitle className="text-3xl font-bold mb-5">Create Organization</DialogTitle>
           </DialogHeader>
-          <CreateOrganizationForm setOpenDialog={setOpenDialog} />
+          <CreateOrganizationForm id={id} setOpenDialog={setOpenDialog} />
         </DialogContent>
       </Dialog>
     );
-
 export default HeaderCreateOrganization;
