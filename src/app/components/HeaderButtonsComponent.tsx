4a13e9446a111bb04de8c10368e9ac46846675b5
Szymon <joceksz@wp.pl>
2024-03-09 19:09:53 +0100

    when user is logged in successfully than redirect him on /files page

--- src/app/components/HeaderButtonsComponent.tsx
+++ src/app/components/HeaderButtonsComponent.tsx
@@ -4,10 +4,25 @@ import React from 'react';
 import { signIn } from 'next-auth/react';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import { toast } from '@/components/ui/use-toast';
 import google from '../../../public/assets/google.svg';
 
 const HeaderButtonsComponent = () => {
-  const signInButton = () => signIn('google');
+  const signInButton = async () => {
+    try {
+      await signIn('google', {
+        redirect: true,
+        callbackUrl: '/files',
+      });
+    } catch (err) {
+      toast({
+        variant: 'destructive',
+        title: 'Problem with logging',
+        description: 'There was an error logging in. Please try again',
+      });
+    }
+  };
+
   return (
     <Button variant="header" onClick={signInButton}>
       <Image className="w-8 mr-2" src={google} alt="google" />
