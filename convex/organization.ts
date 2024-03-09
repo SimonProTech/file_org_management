d4f80faf2a5e188d008638ae9bd4ec6d36e233dc
Szymon <joceksz@wp.pl>
2024-03-09 19:09:03 +0100

    added some functions for organization and used that in UserHeader

--- convex/organization.ts
+++ convex/organization.ts
@@ -1,16 +1,39 @@
-import { v } from 'convex/values';
+import { ConvexError, v } from 'convex/values';
 import { mutation, query } from './_generated/server';
+import { Id } from './_generated/dataModel';
+
+export const generateUploadUrl = mutation(async (ctx) => await ctx.storage.generateUploadUrl());
 
 export const createOrg = mutation({
   args: {
     orgName: v.string(),
     fileId: v.id('_storage'),
+    adminId: v.string(),
+
   },
   handler: async (ctx, args) => {
+    const findOrg = await ctx.db.query('organizations')
+      .withIndex('by_orgName', (q) => q.eq('orgName', args.orgName)).first();
+
+    if (findOrg) {
+      throw new ConvexError('Organization already exists');
+    }
+
     await ctx.db.insert('organizations', {
       orgName: args.orgName,
       fileId: args.fileId,
+      adminId: args.adminId,
     });
+  },
+});
+
+export const getOrganization = query({
+  args: {
+    orgId: v.string(),
+  },
+  handler: (ctx, args) => {
+    const task = ctx.db.query('organizations').filter((q) => q.eq(q.field('_id'), args.orgId)).first();
+    return task;
   },
 });
 
@@ -18,5 +41,3 @@ export const getAllOrganization = query({
   args: {},
   handler: async (ctx, args) => ctx.db.query('organizations').collect(),
 });
-
-export const generateUploadUrl = mutation(async (ctx) => await ctx.storage.generateUploadUrl());
--- src/app/components/UserHeader.tsx
+++ src/app/components/UserHeader.tsx
@@ -1,4 +1,6 @@
-import React, { FC } from 'react';
+'use client';
+
+import React, { FC, useState } from 'react';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,57 +11,78 @@ import {
 } from '@/components/ui/dropdown-menu';
 import Image from 'next/image';
 import {
-  File, SettingsIcon, UserIcon,
+  File, SettingsIcon, Users2Icon,
 } from 'lucide-react';
 import HeaderLogout from '@/app/components/HeaderLogout';
 import HeaderOrganization from '@/app/components/HeaderOrganization';
 import { Session } from 'next-auth';
 import Link from 'next/link';
+import useOrganization from '@/app/store/useOrg';
+import { useQuery } from 'convex/react';
+import HeaderIsAdminAddMembers from '@/app/components/HeaderIsAdminAddMembers';
+import { api } from '../../../convex/_generated/api';
 
-const UserHeader: FC<Pick<Session, 'user'>> = ({ user }) => (
-  <div className="flex items-center gap-x-4">
-    <HeaderOrganization role={user?.role as string} />
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Image
-          className="rounded-xl hover:scale-105 transition-all border-white border-2"
-          src={user?.image as string}
-          width={40}
-          height={40}
-          alt="google user icon"
-        />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-96  z-[99999] relative">
-        <DropdownMenuLabel className="p-2 text-md">
-          Personal
-          {' '}
-          <span className="font-bold text-indigo-600 underline">{user?.name}</span>
-          {' '}
-          account
-        </DropdownMenuLabel>
-        <div className="p-2">
-          <span className="text-[14px] text-indigo-600 font-bold">{user?.email}</span>
-        </div>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem className="cursor-pointer hover:bg-indigo-500 hover:text-white p-2 text-md gap-x-2">
-          <UserIcon size="20" />
-          Profile
-        </DropdownMenuItem>
-        <Link href="/files">
+const UserHeader: FC<Pick<Session, 'user'>> = ({ user }) => {
+  const [sheetOpen, setSheetOpen] = useState<boolean>(false);
+  const { organizationId } = useOrganization();
+  const organizationDetails = useQuery(api.organization.getOrganization, { orgId: organizationId } || 'skip');
+
+  const isAdmin = organizationDetails && organizationDetails.adminId === user.id;
+
+  const handleDropDownMenu = () => {
+    setSheetOpen(true);
+  };
+
+  return (
+    <div className="flex items-center gap-x-4">
+      <HeaderOrganization image={user.image as string} id={user.id} role={user?.role as string} />
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Image
+            className="rounded-xl hover:scale-105 transition-all border-white border-2"
+            src={user?.image as string}
+            width={40}
+            height={40}
+            alt="google user icon"
+          />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-96  z-[99999] relative">
+          <DropdownMenuLabel className="p-2 text-md">
+            Personal
+            {' '}
+            <span className="font-bold text-indigo-600 underline">{user?.name}</span>
+            {' '}
+            account
+          </DropdownMenuLabel>
+          <div className="p-2">
+            <span className="text-[14px] text-indigo-600 font-bold">{user?.email}</span>
+          </div>
+          <DropdownMenuSeparator />
+          <Link href="/files">
+            <DropdownMenuItem className="cursor-pointer p-2 text-md gap-x-2">
+              <File size="20" />
+              Files
+            </DropdownMenuItem>
+          </Link>
+          {isAdmin ? (
+            <DropdownMenuItem onClick={handleDropDownMenu} className="cursor-pointer p-2 text-md gap-x-2">
+              <Users2Icon size="20" />
+              <span>Add members to</span>
+              {' '}
+              <span className="font-bold text-indigo-600 underline">{organizationDetails.orgName}</span>
+            </DropdownMenuItem>
+          ) : null}
           <DropdownMenuItem className="cursor-pointer p-2 text-md gap-x-2">
-            <File size="20" />
-            Files
+            <SettingsIcon size="20" />
+            Settings
           </DropdownMenuItem>
-        </Link>
-        <DropdownMenuItem className="cursor-pointer p-2 text-md gap-x-2">
-          <SettingsIcon size="20" />
-          Settings
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <HeaderLogout />
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </div>
-);
+          <DropdownMenuSeparator />
+          <HeaderLogout />
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <HeaderIsAdminAddMembers setSheetOpen={setSheetOpen} sheetOpen={sheetOpen} />
+    </div>
+  );
+};
 
 export default UserHeader;
