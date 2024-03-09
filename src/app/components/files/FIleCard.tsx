b31e4b4122b0c3ae30966205d8170b3408d539e3
Szymon <joceksz@wp.pl>
2024-03-09 19:15:06 +0100

    added more filetypes added function to mark file as favorite, error handling, image handling etc

--- src/app/components/files/FIleCard.tsx
+++ src/app/components/files/FIleCard.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { ReactNode } from 'react';
 import {
   Card,
@@ -10,21 +12,63 @@ import getFileUrl from '@/lib/getUrl';
 import Image from 'next/image';
 import FilesAction from '@/app/components/files/FilesAction';
 import {
-  File, FileTextIcon, GanttChartIcon, ImageIcon, Star,
+  File, FileHeart, FileImage, FileSpreadsheet, FileTextIcon, GanttChartIcon, ImageIcon, Star,
 } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { formatRelative } from 'date-fns';
+import { useMutation } from 'convex/react';
+import { useSession } from 'next-auth/react';
+import { toast } from '@/components/ui/use-toast';
+import { ConvexError } from 'convex/values';
 import { Doc } from '../../../../convex/_generated/dataModel';
+import { api } from '../../../../convex/_generated/api';
 
-const FIleCard = ({ file }: {file: Doc<'files'>}) => {
+const FIleCard = ({ file, favorite }: {file: Doc<'files'>; favorite: boolean}) => {
+  const { data } = useSession();
   const imageUrl = getFileUrl(file.fileId);
+  const addToFav = useMutation(api.favorite.addToFavorite);
+  const unfavorite = useMutation(api.favorite.unfavorite);
 
   const fileType = {
-    image: <ImageIcon />,
+    image: <FileImage />,
     pdf: <FileTextIcon />,
     csv: <GanttChartIcon />,
+    xlsx: <FileSpreadsheet />,
+    svg: <ImageIcon />,
+    doc: <FileTextIcon />,
     default: <File />,
   } as Record<Doc<'files'>['type'], ReactNode>;
+
+  const addToFavorite = async () => {
+    if (favorite) return;
+
+    try {
+      await addToFav({ fileId: file.fileId, orgId: file.orgId, userId: data?.user.id as string });
+      toast({
+        variant: 'success',
+        description: 'Product added successfully to favorites',
+      });
+    } catch (e) {
+      if (e instanceof ConvexError) {
+        toast({
+          variant: 'destructive',
+          description: e.data,
+        });
+        return;
+      }
+      toast({
+        variant: 'destructive',
+        description: 'There was an error while adding to favorites',
+      });
+    }
+  };
+
+  const markFileAsNotFavorite = async () => {
+    await unfavorite({
+      fileId: file.fileId,
+      orgId: file.orgId,
+    });
+  };
 
   return (
     <Card>
@@ -34,21 +78,26 @@ const FIleCard = ({ file }: {file: Doc<'files'>}) => {
           {file.fileName}
         </CardTitle>
         <div className="flex items-center gap-x-2">
-          <Star className="cursor-pointer" />
-          <FilesAction file={file} />
+          {favorite ? (
+            <Star onClick={markFileAsNotFavorite} className="text-yellow-400 fill-amber-500" />
+          ) : (
+            <Star onClick={addToFavorite} className="cursor-pointer " />
+          )}
+          <FilesAction favorite={favorite} imageUrl={imageUrl} file={file} />
         </div>
       </CardHeader>
-      <CardContent className="h-[200px] flex justify-center items-center">
+      <CardContent className="flex min-h-[200px] relative justify-center items-center">
         {file.type === 'image' && (
         <Image
           alt={file.fileName}
-          width="200"
-          height="100"
+          fill
           src={imageUrl}
+          style={{ objectFit: 'contain' }}
         />
         )}
+        {file.type === 'svg' && <FileHeart className="w-20 h-32" />}
         {file.type === 'pdf' && <FileTextIcon className="w-20 h-32" />}
-        {file.type === 'csv' && <FileTextIcon className="w-20 h-32" />}
+        {(file.type === 'csv' || file.type === 'xlsx') && <FileTextIcon className="w-20 h-32" />}
       </CardContent>
       <CardFooter className="flex justify-between">
         <div className="flex gap-2 text-xs text-gray-700 w-40 items-center">
