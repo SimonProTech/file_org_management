d60c39adecbdd6a64fc8772374a24cf316272a54
Szymon <joceksz@wp.pl>
2024-03-09 19:13:01 +0100

    logout component

--- src/app/components/HeaderLogout.tsx
+++ src/app/components/HeaderLogout.tsx
@@ -4,12 +4,22 @@ import React from 'react';
 import { LogOut } from 'lucide-react';
 import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
 import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
-const HeaderLogout = () => (
-  <DropdownMenuItem onClick={() => signOut()} className="cursor-pointer p-2 text-md gap-x-2">
-    <LogOut size="20" />
-    Logout
-  </DropdownMenuItem>
-);
+const HeaderLogout = () => {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await signOut();
+    router.push('/');
+  };
+
+  return (
+    <DropdownMenuItem onClick={handleLogout} className="cursor-pointer p-2 text-md gap-x-2">
+      <LogOut size="20" />
+      Logout
+    </DropdownMenuItem>
+  );
+};
 
 export default HeaderLogout;
