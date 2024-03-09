4e0a825ddf1f57dea449ce21cf92c019ff63494c
Szymon <joceksz@wp.pl>
2024-03-09 19:16:56 +0100

    refactoring

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,53 +1,7 @@
-import NextAuth from 'next-auth';
+import NextAuth, { Profile } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import { AdapterUser } from 'next-auth/adapters';
+import options from '@/app/api/auth/[...nextauth]/options';
 
-interface CustomUser extends AdapterUser {
-  role: string;
-}
-
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      profile(profile, tokens) {
-        let role = 'member';
-
-        if (profile.email === 'applefunboy98@gmail.com') {
-          role = 'admin';
-        }
-
-        return {
-          id: tokens.id_token || profile.id,
-          ...profile,
-          image: profile.picture,
-          role,
-        };
-      },
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  session: {
-    strategy: 'jwt',
-  },
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        if ('role' in user) {
-          token.role = (user as CustomUser).role;
-        }
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (token) {
-        if ('role' in token) {
-          session.user.role = token.role;
-        }
-      }
-      return session;
-    },
-  },
-});
+const handler = NextAuth(options);
 
 export { handler as GET, handler as POST };
