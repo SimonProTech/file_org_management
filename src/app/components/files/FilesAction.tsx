652300c0cbb178916dbe5912c4c54245bce9edc1
Szymon <joceksz@wp.pl>
2024-03-09 19:16:16 +0100

    file types for searching + added some functionality for deleting

--- src/app/components/files/FilesAction.tsx
+++ src/app/components/files/FilesAction.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState } from 'react';
 import {
   DropdownMenu,
@@ -5,7 +7,9 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { DownloadIcon, MoreVertical, Trash2Icon } from 'lucide-react';
+import {
+  DownloadIcon, Eye, MoreVertical, Trash2Icon, Undo2Icon,
+} from 'lucide-react';
 import {
   AlertDialog,
   AlertDialogAction,
@@ -15,50 +19,128 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
+import { useMutation } from 'convex/react';
+import { toast } from '@/components/ui/use-toast';
 import { Doc } from '../../../../convex/_generated/dataModel';
+import { api } from '../../../../convex/_generated/api';
 
-const FilesAction = ({ file }: {file: Doc<'files'>}) => {
-  const [deleteConfirmation, setDeleteConfirmation] = useState<boolean>(false);
+const FilesAction = ({ file, imageUrl, favorite }: {file: Doc<'files'>, imageUrl: string, favorite: boolean}) => {
+  const permDelete = useMutation(api.files.permDelete);
+  const restoreFileFromDb = useMutation(api.files.restoreFile);
+  const [open, setOpen] = useState<boolean>(false);
+
+  const openView = () => {
+    window.open(imageUrl, '_blank');
+  };
+
+  const restoreFile = async () => {
+    try {
+      await restoreFileFromDb({
+        id: file._id,
+        fileId: file.fileId,
+        orgId: file.orgId,
+      });
+      toast({
+        variant: 'success',
+        description: 'File was restored successfully',
+      });
+    } catch (e) {
+      toast({
+        variant: 'destructive',
+        description: 'There was an error restoring the file, please try again later',
+      });
+    }
+  };
+
+  const fileDeleteFunction = async () => {
+    try {
+      await permDelete({
+        id: file._id,
+        fileId: file.fileId,
+      });
+      toast({
+        variant: 'success',
+        description: 'File was moved to delete folder.',
+      });
+    } catch (e) {
+      toast({
+        variant: 'destructive',
+        description: 'Delete file failed, please try again.',
+      });
+    }
+  };
+
   return (
     <>
       <DropdownMenu>
         <DropdownMenuTrigger><MoreVertical /></DropdownMenuTrigger>
         <DropdownMenuContent className="p-2">
-          <DropdownMenuItem className="gap-x-2 font-medium mb-2 cursor-pointer">
-            <DownloadIcon className="text-green-600" />
-            Download
-          </DropdownMenuItem>
-          <DropdownMenuItem
-            onClick={() => {
-              setDeleteConfirmation(true);
-            }}
-            className="gap-x-2 font-medium cursor-pointer"
-          >
-            <Trash2Icon className="text-red-500" />
-            Delete
-          </DropdownMenuItem>
+          {!file.shouldBeDeleted ? (
+            <DropdownMenuItem className="gap-x-2 font-medium mb-2 cursor-pointer">
+              <Eye className="text-indigo-600" />
+              View
+            </DropdownMenuItem>
+          ) : null}
+          {!file.shouldBeDeleted ? (
+            <DropdownMenuItem onClick={openView} className="gap-x-2 font-medium mb-2 cursor-pointer">
+              <DownloadIcon className="text-green-600" />
+              Download
+            </DropdownMenuItem>
+          ) : null}
+          {file.shouldBeDeleted && (
+            <DropdownMenuItem
+              onClick={() => {
+                restoreFile();
+              }}
+              className="gap-x-2 font-medium cursor-pointer"
+            >
+              <Undo2Icon className="text-green-500" />
+              Restore
+            </DropdownMenuItem>
+          )}
+          {!favorite ? (
+            <DropdownMenuItem
+              onClick={() => {
+                setOpen(true);
+              }}
+              className="gap-x-2 font-medium cursor-pointer"
+            >
+              <Trash2Icon className="text-red-500" />
+              Delete
+            </DropdownMenuItem>
+          ) : null}
         </DropdownMenuContent>
       </DropdownMenu>
-      <AlertDialog open={deleteConfirmation} onOpenChange={setDeleteConfirmation}>
+      <AlertDialog open={open} onOpenChange={setOpen}>
         <AlertDialogContent>
           <AlertDialogHeader>
             <AlertDialogTitle>
-              Do you want to delete
+              Do you want to
+              {' '}
+              delete
               {' '}
               <strong className="underline text-red-600">{file.fileName}</strong>
               {' '}
               ?
             </AlertDialogTitle>
             <AlertDialogDescription className="text-black">
-              This action will mark the file for our deletion process. Files are
-              deleted periodically
+              {file.shouldBeDeleted ? <span>This action will delete the file permanently!</span> : (
+                <span>
+                  This action will mark the file for our deletion process. Files are
+                  deleted periodically
+                </span>
+              )}
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
             <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction className="bg-red-600 hover:bg-red-600">Delete</AlertDialogAction>
+            <AlertDialogAction
+              onClick={fileDeleteFunction}
+              className="bg-red-600 hover:bg-red-600"
+            >
+              Delete
+            </AlertDialogAction>
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
---
+++ src/app/components/files/FilesTypes.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import useOrganization from '@/app/store/useOrg';
+import { useSession } from 'next-auth/react';
+import { useQuery } from 'convex/react';
+import { v4 as uuid } from 'uuid';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import useType from '@/app/store/useType';
+import { api } from '../../../../convex/_generated/api';
+
+const FilesTypes = () => {
+  const { organizationId } = useOrganization();
+  const user = useSession();
+  const filesType = useQuery(api.files.filesType, { orgId: organizationId || user.data?.user.id as string });
+  const setType = useType((state) => state.setType);
+
+  return (
+    <div>
+      <Select onValueChange={setType}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select a file type" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>File types</SelectLabel>
+            <SelectItem value="all">All</SelectItem>
+            {filesType?.map((type) => (
+              <SelectItem key={uuid()} value={type}>{type}</SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+
+    </div>
+  );
+};
+
+export default FilesTypes;
