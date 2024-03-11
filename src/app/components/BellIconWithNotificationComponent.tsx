d34b3bd69febb86730379d79f4481c8b5c590ab0
Simon <szymon98181@gmail.com>
2024-03-11 22:17:50 +0100

    working on notifications

--- src/app/components/BellIconWithNotificationComponent.tsx
+++ src/app/components/BellIconWithNotificationComponent.tsx
@@ -1,4 +1,6 @@
-import React, { FC } from 'react';
+'use client';
+
+import React, { FC, useState } from 'react';
 import {
   Drawer,
   DrawerContent,
@@ -7,32 +9,59 @@ import {
   DrawerTrigger,
 } from '@/components/ui/drawer';
 import { Bell } from 'lucide-react';
-import { useQuery } from 'convex/react';
+import { useMutation, useQuery } from 'convex/react';
+import { Button } from '@/components/ui/button';
+import { toast } from '@/components/ui/use-toast';
 import { api } from '../../../convex/_generated/api';
+import { Id } from '../../../convex/_generated/dataModel';
 
 interface BellIconWithNotificationComponentProps {
   userEmail: string;
-  userId: string;
+  userGoogleId: Id<'user'>;
+  userImage: string;
 }
 
 const BellIconWithNotificationComponent:
-    FC<BellIconWithNotificationComponentProps> = ({ userId, userEmail }) => {
+    FC<BellIconWithNotificationComponentProps> = ({ userEmail, userImage, userGoogleId }) => {
+      const [openDrawer, setOpenDrawer] = useState(false);
       const getUserAddedToOrganization = useQuery(api.user.getUserAddedToOrganization, {
-        userId,
         userEmail,
-      } || 'skip');
+      });
 
-      console.log(getUserAddedToOrganization);
+      const joinOrganization = useMutation(api.user.joinOrganization);
+
+      const joinOrg = async (id: Id<'user'>) => {
+        try {
+          await joinOrganization({
+            userId: id,
+            id: userGoogleId,
+            userImage,
+          });
+
+          setOpenDrawer(false);
+
+          toast({
+            variant: 'success',
+            description: 'You joined the organization',
+          });
+        } catch (err) {
+          console.log(err);
+          toast({
+            variant: 'destructive',
+            description: 'Error while joining the organization',
+          });
+        }
+      };
 
       return (
-        <Drawer activeSnapPoint={50} direction="bottom">
+        <Drawer open={openDrawer} onOpenChange={setOpenDrawer} activeSnapPoint={50} direction="bottom">
           <DrawerTrigger className="relative flex items-center">
-            {/* {getUserAddedToOrganization && getUserAddedToOrganization.length > 0 ? ( */}
-            {/*  <> */}
-            {/*    <span className="animate-ping bg-gray-700 absolute inline-flex right-0 w-3 -top-1 h-3 rounded-full opacity-75" /> */}
-            {/*    <span className="absolute right-0 -top-1 inline-flex rounded-full h-3 w-3 bg-indigo-600" /> */}
-            {/*  </> */}
-            {/* ) : null} */}
+            {getUserAddedToOrganization && getUserAddedToOrganization.length > 0 ? (
+              <>
+                <span className="animate-ping bg-gray-700 absolute inline-flex right-0 w-3 -top-1 h-3 rounded-full opacity-75" />
+                <span className="absolute right-0 -top-1 inline-flex rounded-full h-3 w-3 bg-indigo-600" />
+              </>
+            ) : null}
             <Bell className="cursor-pointer" />
           </DrawerTrigger>
           <DrawerContent className="p-5">
@@ -40,8 +69,20 @@ const BellIconWithNotificationComponent:
               <DrawerTitle className="text-indigo-600 text-2xl">Notifications</DrawerTitle>
             </DrawerHeader>
             <div className="pb-10">
-              <p className="mb-2">1</p>
-              <p>1</p>
+              {getUserAddedToOrganization?.map((user) => (
+                <div className="flex gap-x-2 items-center">
+                  <p>
+                    You have been invited to the
+                    {' '}
+                    <span className="font-bold text-indigo-600">{user?.organization?.orgName}</span>
+                    {' '}
+                    organization by
+                    {' '}
+                    <span className="font-bold underline">{user?.organization?.adminName}</span>
+                  </p>
+                  <Button onClick={() => joinOrg(user._id)} className="bg-orange-500 hover:bg-orange-600">Join now</Button>
+                </div>
+              ))}
             </div>
           </DrawerContent>
         </Drawer>
