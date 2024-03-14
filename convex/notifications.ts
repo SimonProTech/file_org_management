65574cc07e3d209a337191f75ae0120f9efdc818
Simon <szymon98181@gmail.com>
2024-03-14 18:07:26 +0100

    mark as read function remove if all users saw that notification

--- convex/notifications.ts
+++ convex/notifications.ts
@@ -1,17 +1,22 @@
-import { v } from 'convex/values';
-import { mutation, query } from './_generated/server';
+import { ConvexError, v } from 'convex/values';
+import { createMetadataExportsCode } from 'next/dist/build/webpack/loaders/metadata/discover';
+import { internalMutation, mutation, query } from './_generated/server';
+import { Doc } from './_generated/dataModel';
 
 export const createNotification = mutation({
   args: {
     orgId: v.id('organizations'),
     message: v.string(),
-    userId: v.string(),
+    users: v.array(v.object({
+      id: v.string(),
+      wasRead: v.boolean(),
+    })),
   },
   handler: async (ctx, args) => ctx.db.insert('notifications', {
     orgId: args.orgId,
     message: args.message,
     wasRead: false,
-    userId: args.userId,
+    users: args.users,
   }),
 });
 
@@ -25,20 +30,63 @@ export const getNotifications = query({
       return [];
     }
 
-    return ctx.db.query('notifications')
-      .filter((q) => q.eq(q.field('userId'), args.userId))
+    const notifications = await ctx.db.query('notifications')
       .filter((q) => q.eq(q.field('orgId'), args.orgId))
-      .filter((q) => q.eq(q.field('wasRead'), false))
+      .order('desc')
       .collect();
+
+    const hasUnreadNotifications = notifications.filter((x) => x.users.some((d) => d.id === args.userId));
+
+    return hasUnreadNotifications;
   },
 });
 
-export const markNotificationAsReaded = mutation({
+export const markNotificationAsRode = mutation({
   args: {
+    notificationId: v.id('notifications'),
     userId: v.string(),
-    orgId: v.string(),
   },
   handler: async (ctx, args) => {
-    console.log(args);
+    const notification = await ctx.db.get(args.notificationId);
+    if (!notification) {
+      throw new ConvexError(' Notifications not found');
+    }
+
+    const markWhenUserClick = notification.users.map(async (user) => {
+      await ctx.db.patch(notification._id, {
+        users: [{
+          id: user.id,
+          wasRead: true,
+        }],
+      });
+      return {
+        rode: true,
+      };
+    });
+
+    const markWasReadByAll = notification.users.filter(async (item) => {
+      if (item.wasRead) {
+        return false;
+      }
+      await ctx.db.patch(notification._id, {
+        wasRead: true,
+      });
+      return {
+        allUsersRode: true,
+      };
+    });
+    await Promise.all(markWhenUserClick);
+    await Promise.all(markWasReadByAll);
+    return true;
+  },
+});
+
+export const removeNotificationsAfterOneWeekIfAllUsersSaw = internalMutation({
+  args: {},
+  handler: async (ctx) => {
+    const allNotifications = await ctx.db.query('notifications').filter((q) => q.eq(q.field('wasRead'), true)).collect();
+    return Promise.all(allNotifications.map(
+      async (notification) => ctx.db.delete(notification._id),
+    ));
   },
 });
