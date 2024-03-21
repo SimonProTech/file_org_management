8415f0af03ee086dd9bd387685faccb70a80cb16
Simon <szymon98181@gmail.com>
2024-03-21 12:24:17 +0100

    refactoring

--- @types/next-auth.d.ts
+++ @types/next-auth.d.ts
@@ -4,14 +4,6 @@ import { User } from 'next-auth';
 
 declare module 'next-auth' {
     interface Session {
-        user: User & {
-            role: string;
-        }
-    }
-}
-
-declare module 'next-auth/jwt' {
-    interface JWT {
-        role?: string;
+        user: User
     }
 }
