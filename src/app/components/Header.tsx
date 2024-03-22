15ec6e6b0d7cc1e0d74b0c52b5c7b3c4c39e411d
Simon <szymon98181@gmail.com>
2024-03-22 17:05:40 +0100

    added new logo to the header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
 import HeaderButtonsComponent from '@/app/components/HeaderButtonsComponent';
-import { FileBadge } from 'lucide-react';
 import { getServerSession } from 'next-auth';
 import UserHeader from '@/app/components/UserHeader';
 import options from '@/app/api/auth/[...nextauth]/options';
+import Image from 'next/image';
 
 const Header = async () => {
   const session = await getServerSession(options);
   return (
     <div className="p-2 fixed left-1/2 max-w-lg w-full z-[99999] -translate-x-1/2">
-      <div className="flex bg-white border-white border-4 shadow-xl mx-auto w rounded-3xl p-3">
+      <div className="flex bg-white border-indigo-600 border-2 mx-auto w rounded-3xl p-3">
         <div className="flex justify-between text-black w-full items-center">
           <Link className="flex gap-x-2 font-bold text-lg items-center" href="/">
-            <FileBadge />
-            TeamFileOrg
+            <Image width={140} height={100} src="/assets/logo.svg" alt="xd" />
           </Link>
           {!session ? (
             <HeaderButtonsComponent />
