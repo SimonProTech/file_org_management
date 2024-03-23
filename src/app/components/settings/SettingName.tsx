639709cd9ae0f00c56234c15dcd2c47c6c8677f6
Simon <szymon98181@gmail.com>
2024-03-23 11:03:41 +0100

    refactoring

--- src/app/components/settings/SettingName.tsx
+++ src/app/components/settings/SettingName.tsx
@@ -1,9 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
-import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card';
-import useUser from '@/app/store/useUser';
-import { Roles } from '@/app/types/types';
+import React, { useState } from 'react';
 import { PencilLineIcon } from 'lucide-react';
 import {
   Dialog,
@@ -18,52 +15,31 @@ import { Id } from '../../../../convex/_generated/dataModel';
 const SettingName = ({
   name,
   sessionId,
-  orgFounderId,
   organizationId,
-} : {name: string, sessionId: string, orgFounderId: string, organizationId: Id<'organizations'>}) => {
-  const { role } = useUser();
-  const setRole = useUser((state) => state.setRole);
+} : {name: string, sessionId: string, organizationId: Id<'organizations'>}) => {
   const [dialogOpen, setDialogOpen] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (orgFounderId === sessionId) {
-      setRole(Roles.admin);
-    }
-  }, [orgFounderId, sessionId]);
 
   return (
     <div className="flex items-center gap-x-5">
-      {role === Roles.admin ? (
-        <Dialog
-          open={dialogOpen}
-          onOpenChange={setDialogOpen}
-        >
-          <DialogTrigger>
-            <PencilLineIcon className="text-green-600 cursor-pointer" size={40} />
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle className="text-xl">Do you want to change organization name?</DialogTitle>
-              <SettingChangeName
-                adminId={sessionId}
-                orgId={organizationId}
-                setDialogOpen={setDialogOpen}
-                orgName={name}
-              />
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
-      ) : null}
-      <HoverCard>
-        <HoverCardTrigger>
-          <span className="underline font-bold cursor-pointer text-xl">
-            {name}
-          </span>
-        </HoverCardTrigger>
-        <HoverCardContent>
-          Name of the organization.
-        </HoverCardContent>
-      </HoverCard>
+      <Dialog
+        open={dialogOpen}
+        onOpenChange={setDialogOpen}
+      >
+        <DialogTrigger>
+          <PencilLineIcon className="text-white hover:scale-105 transition-all cursor-pointer" size={30} />
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="text-xl">Do you want to change organization name?</DialogTitle>
+            <SettingChangeName
+              adminId={sessionId}
+              orgId={organizationId}
+              setDialogOpen={setDialogOpen}
+              orgName={name}
+            />
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
