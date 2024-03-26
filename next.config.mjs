6b0bd19e37343a3b806b034827527c478a4ff3c6
Simon <szymon98181@gmail.com>
2024-03-26 17:52:12 +0100

    add hostname to next config :)

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,16 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
+                protocol: 'https',
                 hostname: "lh3.googleusercontent.com"
             },
             {
+                protocol: 'https',
                 hostname: 'effervescent-goshawk-546.convex.cloud'
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.buymeacoffee.com'
             }
         ]
     }
