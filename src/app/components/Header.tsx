36de0872d01d5414807789f8417e73a67cbc9a0f
Simon <szymon98181@gmail.com>
2024-03-27 17:28:22 +0100

    still working on z-index problems

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -9,7 +9,7 @@ import Image from 'next/image';
 const Header = async () => {
   const session = await getServerSession(options);
   return (
-    <div className="p-2 fixed left-1/2 max-w-lg w-full z-[99999999999999] -translate-x-1/2">
+    <div className="p-2 fixed left-1/2 max-w-lg w-full z-[999999999] -translate-x-1/2">
       <div className="flex bg-white border-indigo-600 border-2 mx-auto w rounded-3xl p-3">
         <div className="flex justify-between text-black w-full items-center">
           <Link className="flex gap-x-2 font-bold text-lg items-center" href="/">
