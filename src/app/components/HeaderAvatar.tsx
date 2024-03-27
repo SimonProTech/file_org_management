170df7b18c606cf4e9cd779932c5f0c764ad5d04
Simon <szymon98181@gmail.com>
2024-03-27 17:28:56 +0100

    zoom avatar when user hover over it

--- src/app/components/HeaderAvatar.tsx
+++ src/app/components/HeaderAvatar.tsx
@@ -4,6 +4,11 @@ import { SelectItem } from '@/components/ui/select';
 import { useQuery } from 'convex/react';
 import randomColorPick from '@/lib/randomColorPick';
 import { Loader2 } from 'lucide-react';
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from '@/components/ui/hover-card';
 import { api } from '../../../convex/_generated/api';
 import { Id } from '../../../convex/_generated/dataModel';
 
@@ -17,10 +22,22 @@ const HeaderAvatar = ({ fileId, orgName, id }: {fileId: Id<'_storage'>, orgName:
       {imageUrl === undefined ? (
         <Loader2 size={50} className="animate-spin" />
       ) : (
-        <Avatar>
-          <AvatarImage width={30} height={30} src={imageUrl!} alt={`${orgName.slice(0, 2)}...`} />
-          <AvatarFallback className={`${randomColorPick()} text-white`}>{orgName.slice(0, 2)}</AvatarFallback>
-        </Avatar>
+        <HoverCard>
+          <HoverCardTrigger asChild>
+            <Avatar>
+              <AvatarImage width={30} height={30} src={imageUrl!} alt={`${orgName.slice(0, 2)}...`} />
+              <AvatarFallback className={`${randomColorPick()} text-white`}>{orgName.slice(0, 2)}</AvatarFallback>
+            </Avatar>
+          </HoverCardTrigger>
+          <HoverCardContent side="right" className="w-max">
+            <div className="flex justify-between space-x-4">
+              <Avatar className="w-20 h-20">
+                <AvatarImage width={40} height={40} src={imageUrl as string} />
+                <AvatarFallback>{orgName.slice(0, 2)}</AvatarFallback>
+              </Avatar>
+            </div>
+          </HoverCardContent>
+        </HoverCard>
       )}
       <SelectItem
         value={id}
