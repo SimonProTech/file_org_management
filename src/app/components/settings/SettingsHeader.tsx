cf14bf5c7d3ba48e281056f914fc0973a9048a1f
Simon <szymon98181@gmail.com>
2024-03-28 16:37:03 +0100

    made some UI changes

--- src/app/components/settings/SettingsHeader.tsx
+++ src/app/components/settings/SettingsHeader.tsx
@@ -4,6 +4,7 @@ import React, { FC } from 'react';
 import useOrganization from '@/app/store/useOrg';
 import { useQuery } from 'convex/react';
 import { useParams } from 'next/navigation';
+import { Cog } from 'lucide-react';
 import { api } from '../../../../convex/_generated/api';
 
 interface SettingsHeaderProps {
@@ -19,11 +20,19 @@ const SettingsHeader: FC<SettingsHeaderProps> = ({ title }) => {
   });
 
   return (
-    <h1 className="text-black font-bold text-5xl">
-      Settings
-      {' '}
-      {getOrganization ? `- ${getOrganization.orgName}` : title }
-    </h1>
+    <div className="flex md:flex-row flex-col gap-x-2 items-center">
+      <Cog className="hover:rotate-45 transition-all md:mb-0 mb-5 sm:w-14 sm:h-15 w-12 h-12" />
+      <h1 className="text-black font-asap text-5xl">
+        Settings
+      </h1>
+      <span className="text-black font-asap md:rotate-0 rotate-90 text-5xl">
+        {' '}
+        -
+      </span>
+      <span className="text-black font-asap underline decoration-wavy font-bold text-3xl sm:text-5xl">
+        {getOrganization ? `${getOrganization.orgName}` : title }
+      </span>
+    </div>
   );
 };
 
