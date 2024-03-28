9b17e5befa80d8ddcd3e344ee395ef99d6dcabf5
Simon <szymon98181@gmail.com>
2024-03-28 16:37:17 +0100

    redirect when you are not logged in

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import SettingsHeader from '@/app/components/settings/SettingsHeader';
+import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth';
+import options from '@/app/api/auth/[...nextauth]/options';
 
-const Page = () => (
-  <div>
-    <SettingsHeader title="(Personal account)" />
-  </div>
-);
+const Page = async () => {
+  const session = await getServerSession(options);
 
+  if (!session) {
+    redirect('/');
+  }
+  return (
+    <div>
+      <SettingsHeader title="(Personal account)" />
+    </div>
+  );
+};
 export default Page;
