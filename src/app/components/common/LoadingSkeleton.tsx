d1d7b6612a275c6c6f29d4aaf51507ba31d01d62
Simon <szymon98181@gmail.com>
2024-03-28 16:48:04 +0100

    more reusable skeleton

--- src/app/components/common/LoadingSkeleton.tsx
+++ src/app/components/common/LoadingSkeleton.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Skeleton } from '@/components/ui/skeleton';
+import { cn } from '@/lib/utils';
+import { v4 as uuid } from 'uuid';
 
-const LoadingSkeleton = () => (
+const LoadingSkeleton = ({ className }: {className?: string}) => (
   <div className="pt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
     {[...Array(4)].map(() => (
-      <Skeleton className="h-20 bg-gray-300 w-full rounded-md p-3 flex items-center justify-between" />
+      <Skeleton
+        key={uuid()}
+        className={cn('h-14 bg-gray-300 w-full rounded-md p-3 flex items-center justify-between', className)}
+      />
     ))}
   </div>
 );
